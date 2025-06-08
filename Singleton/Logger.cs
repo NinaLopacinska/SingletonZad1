e32caa3f261e626790f3936b0d0156274fbebfdc
Nina Łopacińska <UL0273299@edu.uni.lodz.pl>
2025-06-08 11:17:13 +0200

    wzorzec Singleton, lazy i lock

---
+++ Singleton/Logger.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonMOJ
+{
+    public sealed class Logger
+    {
+        //private static Logger _instance = null;
+
+
+
+        // Obiekt do synchronizacji (dla bezpieczeństwa wątkowego)
+        //private static readonly object _lock = new object();
+
+        // Lazy<T> automatycznie dba o thread-safety i leniwe tworzenie instancji
+        private static readonly Lazy<Logger> _instance = new Lazy<Logger>(() => new Logger());
+
+        private Logger()//nikt poza klasą Logger nie może tworzyć instancji 
+        {
+            Console.WriteLine("Logger został utworzony.");
+        }
+
+        public static Logger Instance
+        {
+            get
+            {
+                //if (_instance == null)
+                //{
+                //lock (_lock)
+                //{
+                //if (_instance ==null)
+                // {
+                //_instance = new Logger();
+                // }
+                //}
+                //_instance = new Logger();
+
+                return _instance.Value;// Tworzenie przy pierwszym użyciu
+
+                //}
+                //return _instance;
+            }
+        }
+        
+        public void Log(string message)
+        {
+            Console.WriteLine( $"[LOG]: {message}");
+        }
+        
+
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,29 @@
+﻿using SingletonMOJ;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.Instance.Log("Pierwszy Log");
+
+            Thread t1 = new Thread(() => Logger.Instance.Log("Log z wątku 1"));
+            Thread t2 = new Thread(() => Logger.Instance.Log("Log z wątku 2"));
+
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+
+            Logger.Instance.Log("Oststni log");
+            Console.ReadKey();
+            
+        }
+
+    }
+}
+
+
+
